5f59fa35bcc8edc3c10823eee259b819f4152b55
MC1854 <marysiaciosk1@gmail.com>
2024-12-12 12:32:03 +0100

    Initial commit

---
+++ Obstakel.cs
@@ -0,0 +1,9 @@
+using System;  // Dodaj to na początku pliku, jeśli brakuje
+
+public class Obstakel
+{
+    public int Xpos { get; set; }
+    public int Ypos { get; set; }
+    public ConsoleColor schermKleur { get; set; }
+    public string karacter { get; set; }
+}
---
+++ Pixel.cs
@@ -0,0 +1,9 @@
+using System;  // Dodaj to na początku pliku, jeśli brakuje
+
+public class Pixel
+{
+    public int xPos { get; set; }
+    public int yPos { get; set; }
+    public ConsoleColor schermKleur { get; set; }
+    public string karacter { get; set; }
+}
